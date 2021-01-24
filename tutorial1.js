a93cb48deec1f347cc988e55bd2cc51a50c08dd8
James Serendip <james.serendip@flemingcollege.ca>
2021-01-24 12:14:11 -0800

    change location to old home

---
+++ tutorial1.js
@@ -0,0 +1,16 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 40.77282767944372, lng: -73.01558827273769 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
