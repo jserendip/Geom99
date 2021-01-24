7d770544e9705f6c40b71b678df5e4085ddd19fc
James Serendip <james.serendip@flemingcollege.ca>
2021-01-24 12:32:18 -0800

    add markercluster script

--- tutorial1.js
+++ tutorial1.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 40.77282767944372, lng: -73.01558827273769 };
-    // The map, centered at Uluru
+    // The location of my ancetral home in New York
+    const patchoguehouse = { lat: 40.77282767944372, lng: -73.01558827273769 };
+    // The map, centered at old home
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
-      center: uluru,
+      center: patchoguehouse,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at 264 North Ocean Ave. Patchogue NY
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: patchoguehouse,
       map: map,
     });
   }
