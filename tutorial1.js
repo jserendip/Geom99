6a6f69130d3fe823e55933906f4251b329eda2c4
James Serendip <james.serendip@flemingcollege.ca>
2021-01-24 12:14:44 -0800

    update zoom

--- tutorial1.js
+++ tutorial1.js
@@ -4,7 +4,7 @@ function initMap() {
     const uluru = { lat: 40.77282767944372, lng: -73.01558827273769 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 12,
       center: uluru,
     });
     // The marker, positioned at Uluru
