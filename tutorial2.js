7e29619976d3518587cfdc39472456517c366a04
James Serendip <james.serendip@flemingcollege.ca>
2021-01-24 13:05:06 -0800

    update home locations

--- tutorial2.js
+++ tutorial2.js
@@ -4,7 +4,7 @@ function initMap() {
     const patchoguehouse = { lat: 40.77282767944372, lng: -73.01558827273769 };
     // The map, centered at old home
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
+      zoom: 10,
       center: patchoguehouse,
     });
     // The marker, positioned at 264 North Ocean Ave. Patchogue NY
@@ -31,29 +31,13 @@ function initMap() {
         "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
   }
-  const locations = [  
+  const locations = [      
     { lat: 40.75043120704643, lng: -73.00752451016817 },
     { lat: 40.756347936794874, lng: -73.01679422464042 },
     { lat: 40.74731008253978, lng: -73.02640726187092 },
-    { lat: -33.848588, lng: 151.209834 },
-    { lat: -33.851702, lng: 151.216968 },
-    { lat: -34.671264, lng: 150.863657 },
-    { lat: -35.304724, lng: 148.662905 },
-    { lat: -36.817685, lng: 175.699196 },
-    { lat: -36.828611, lng: 175.790222 },
-    { lat: -37.75, lng: 145.116667 },
-    { lat: -37.759859, lng: 145.128708 },
-    { lat: -37.765015, lng: 145.133858 },
-    { lat: -37.770104, lng: 145.143299 },
-    { lat: -37.7737, lng: 145.145187 },
-    { lat: -37.774785, lng: 145.137978 },
-    { lat: -37.819616, lng: 144.968119 },
-    { lat: -38.330766, lng: 144.695692 },
-    { lat: -39.927193, lng: 175.053218 },
-    { lat: -41.330162, lng: 174.865694 },
-    { lat: -42.734358, lng: 147.439506 },
-    { lat: -42.734358, lng: 147.501315 },
-    { lat: -42.735258, lng: 147.438 },
-    { lat: -43.999792, lng: 170.463352 },
+    { lat: 40.761145463908356, lng: -73.01227249429115 },
+    { lat: 40.76011339955494, lng: -73.01160730644708 },
+    { lat: 40.76189309106897, lng: -73.01345266627257 },
+    { lat: 40.776990109564196, lng: -73.0071119241431 },
   ];
   
