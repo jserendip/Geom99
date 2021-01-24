dace27235aeb4345104ac70f0c7f8f018b7490cc
James Serendip <james.serendip@flemingcollege.ca>
2021-01-24 10:49:22 -0800

    zoom reset

--- jscontent.js
+++ jscontent.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 48.63619811297952, lng: -1.5112934203748902 },
-    zoom: 8,
+    zoom: 14,
   });
 }
