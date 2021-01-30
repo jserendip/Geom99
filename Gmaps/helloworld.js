bce95b76f77821d82d322e8bf6a81a24d907f628
James Serendip <james.serendip@flemingcollege.ca>
2021-01-29 16:16:35 -0800

    hellopatchoguehouse

--- Gmaps/helloworld.js
+++ Gmaps/helloworld.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 40.772876, lng: -73.015550 },
-    zoom: 14,
+    zoom: 11,
   });
 }
